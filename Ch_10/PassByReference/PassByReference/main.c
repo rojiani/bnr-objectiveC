1700a1b52cf068434744bb2e0032b5cf62ed489d
Navid Rojiani <nrojiani@gmail.com>
2016-04-17 21:17:26 -0700

    C Review: Pointers & Pass by Reference

--- Ch_10/PassByReference/PassByReference/main.c
+++ Ch_10/PassByReference/PassByReference/main.c
@@ -9,6 +9,29 @@
 #include <stdio.h>
 #include <math.h>
 
+#define FEET_PER_METER 3.28084;
+
+static void meters_to_feet_and_inches(double meters, unsigned int *ft_ptr,
+                                      double *inches_ptr)
+{
+    // Convert the number of meters into a floating-point number of feet
+    double raw_feet = meters * FEET_PER_METER;
+    
+    unsigned int feet = (unsigned int) floor(raw_feet);
+    if (ft_ptr) {
+        *ft_ptr = feet;
+        printf("Storing %u to the address %p\n", feet, ft_ptr);
+    }
+    
+    double fractional_part = raw_feet - feet;
+    double inches = fractional_part * 12.0;
+    if (inches_ptr) {
+        printf("Storing %.2f to the address %p\n", inches, inches_ptr);
+        *inches_ptr = inches;
+    }
+
+}
+
 int main(int argc, const char * argv[])
 {
     double pi = 3.14;
@@ -22,6 +45,16 @@ int main(int argc, const char * argv[])
     
     // Find the value stored in integerPart
     printf("integerPart = %.0f, fractionPart = %.2f\n", integerPart, fractionPart);
+    
+    printf("\n\n");
+    // FEET TO INCHES
+    double meters = 3.0;
+    unsigned int feet;
+    double inches;
+    
+    meters_to_feet_and_inches(meters, &feet, &inches);
+    printf("%.1f meters is equal to %d feet and %.1f inches.\n", meters, feet,
+           inches);
     
     return 0;
 }
